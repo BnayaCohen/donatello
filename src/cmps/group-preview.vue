e50f9a89e5a76cc926bfb884faa3ea4bcff0b1aa
Bnaya <bnaya98@gmail.com>
2022-07-19 18:21:00 +0300

    added board page

---
+++ src/cmps/group-preview.vue
@@ -0,0 +1,18 @@
+<template>
+    <article class="group-preview">
+        
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'group-preview',
+    props: {
+        group: {
+            group: Object,
+        },
+    },
+    methods: {
+    }
+}
+</script>
