1d652feeb0e16402a4c86fb0b7ae1e70b0cee198
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-19 18:19:30 +0300

    added app-header and basic routes

---
+++ src/cmps/app-header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="app-header flex">
+    <section class="nav-container">
+      <nav class="main-nav flex">
+        <router-link to="/">Home</router-link>
+        <router-link to="/board">Boards</router-link>
+      </nav>
+    </section>
+    <section class="logo flex">
+      <h1>Donatello</h1>
+    </section>
+    <section class="user-actions">
+        <button>+</button>
+        <!-- REPLACE WITH RELEVANT CMPS (login-logout, notification) -->
+        <button>Noti</button> 
+        <button>User</button>
+    </section>
+  </header>
+</template>
+
+<script>
+// import loginLogout from '../components/login-logout.vue'
+export default {
+  name: 'appHome',
+  data() {
+    return {}
+  },
+  methods: {
+    login(credentials) {
+      this.$emit('login', credentials)
+    },
+    logout() {
+      this.$emit('logout')
+    },
+    signup(signupInfo) {
+      this.$emit('signup', signupInfo)
+    },
+  },
+  computed: {
+    isAdmin() {
+      return this.$store.getters.isAdmin
+    }
+  },
+  components: {
+    // loginLogout,
+  },
+}
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import homePage from '../views/home-page.vue'
+import boardPage from '../views/board-page.vue'
+import boardDetails from '../views/board-details.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: homePage
+    },
+    {
+      path: '/board',
+      name: 'boardPage',
+      component: boardPage
+    },
+    {
+      path: '/board/:boardId',
+      name: 'boardDetails',
+      component: boardDetails
     },
   ]
 })
