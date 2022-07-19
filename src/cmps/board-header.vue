420a647027acb5a2cfcc831ba3c718909a5ba626
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-19 21:48:36 +0300

    .

--- src/cmps/board-header.vue
+++ src/cmps/board-header.vue
@@ -1,29 +1,27 @@
 <template>
-    <header class="board-header flex justify-between">
-        <section class="board-options">
-            <input type="text" placeholder="Enter board name here">
-            <button>&#9734;</button>
-            <div class="members-container">
-                <p>.....</p>
-                <button>Invite</button>
-            </div>
-        </section>
-        <section class="more-options">
-            <button>Dashboard</button>
-            <button>Show menu</button>
-        </section>
-    </header>
+  <header class="main-layout board-header">
+    <div class="flex justify-between">
+      <section class="board-options flex justify-center">
+        <input class="board-name-input" type="text" placeholder="Enter board name here" />
+        <button>&#9734;</button>
+        <div class="members-container flex align-center">
+          <p>(board members)</p>
+          <button>Invite</button>
+        </div>
+      </section>
+      <section class="more-options">
+        <button>Dashboard</button>
+        <button>Show menu</button>
+      </section>
+    </div>
+  </header>
 </template>
 <script>
 export default {
-    name: 'boardHeader',
-    data() {
-        return {
-
-        }
-    },
+  name: 'boardHeader',
+  data() {
+    return {}
+  },
 }
 </script>
-<style>
-    
-</style>
+<style></style>
