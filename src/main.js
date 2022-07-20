850ae8b65877def16c508f945cfd24bbdf77a75d
Bnaya <bnaya98@gmail.com>
2022-07-20 11:05:26 +0300

    new directives

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { focusDirective, clickOutsideDirective } from './directives'
 
 // Styles
 import "./assets/styles/styles.scss";
 
 const app = createApp(App)
+
+app.directive('focus', focusDirective)
+app.directive('click-outside', clickOutsideDirective)
 
 app.use(router)
 app.use(store)
