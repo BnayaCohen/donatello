072b70d4ff758ac5ff9957879ab64687c0a05a24
Bnaya <bnaya98@gmail.com>
2022-07-20 15:01:03 +0300

    add group

--- src/cmps/group-preview.vue
+++ src/cmps/group-preview.vue
@@ -2,7 +2,8 @@
     <article v-if="group.title" class="group-container flex flex-column">
         <div class="group-header flex justify-between align-center">
             <input class="group-title clean-input" type="text" v-model="group.title"
-                placeholder="Enter group title..." @keyup.enter="updateTitle">
+                placeholder="Enter list title..." @keyup.enter="updateTitle">
+
             <button class="group-options-btn"><svg stroke="currentColor" fill="currentColor" stroke-width="0"
                     viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
@@ -10,18 +11,25 @@
                         clip-rule="evenodd"></path>
                 </svg></button>
         </div>
+
         <task-list :tasks="group.tasks" />
+
         <button @click="addTask" class="add-card-btn flex align-center">
-            <i class="plus-sign"><svg stroke="currentColor" fill="currentColor" stroke-width="0" t="1551322312294"
-                    viewBox="0 0 1024 1024" version="1.1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <defs></defs>
-                    <path d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z"></path>
-                    <path d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z"></path>
-                </svg>
-            </i>
+            <i class="plus-sign"><svg stroke="currentColor" fill="currentColor" stroke-width="0" t="1551322312294" viewBox="0 0 1024 1024" version="1.1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><defs></defs><path d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z"></path><path d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z"></path></svg></i>
             Add a card
         </button>
     </article>
+
+    <!-- <article v-else class="add-group-container flex align-center" :class="addingGroup">
+        <span  v-if="!isNewGroupOpen" class="add-group-placeholder flex align-center justify-center">
+            <i class="plus-sign"><svg stroke="currentColor" fill="currentColor" stroke-width="0" t="1551322312294" viewBox="0 0 1024 1024" version="1.1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><defs></defs><path d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z"></path><path d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z"></path></svg></i>
+            Add another list
+        </span>
+        <section v-if="isNewGroupOpen" class="">
+ <input class="group-title clean-input" type="text" v-model="group.title"
+                placeholder="Enter list title..." @keyup.enter="updateTitle">
+        </section>
+    </article> -->
 </template>
 
 <script>
@@ -33,15 +41,22 @@ export default {
     },
     data() {
         return {
+            // isNewGroupOpen:false,
         }
     },
     methods: {
         updateTitle() {
+            if(this.group.title==='') return
             this.$store.dispatch({ type: 'saveGroup', group: this.group })
         },
         addTask(groupId) {
             this.$emit('add-task', groupId)
         }
+    },
+    computed:{
+// addingGroup(){
+//     return {active:this.isNewGroupOpen}
+// }
     },
     components: {
         taskList,
