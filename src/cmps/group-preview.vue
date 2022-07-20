474600ec95371fd0ff51826a98859da27807acdb
Bnaya <bnaya98@gmail.com>
2022-07-20 20:33:57 +0300

    addin ggroups complete

--- src/cmps/group-preview.vue
+++ src/cmps/group-preview.vue
@@ -1,6 +1,6 @@
 <template>
     <article v-if="group.title" class="group-container flex flex-column">
-        <div class="group-header flex justify-between align-center">
+        <div class="group-header flex">
             <input class="group-title clean-input" type="text" v-model="group.title" placeholder="Enter list title..."
                 @keyup.enter="updateTitle">
 
@@ -13,22 +13,14 @@
         </div>
 
         <task-list :tasks="group.tasks" />
-
         <section class="add-task-container">
             <add-group-or-task :groupOrTask="'task'" :groupId="group.id" />
         </section>
-        <!-- <button @click="addTask" class="add-card-btn flex align-center">
-            <i class="plus-sign"><svg stroke="currentColor" fill="currentColor" stroke-width="0" t="1551322312294"
-                    viewBox="0 0 1024 1024" version="1.1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <defs></defs>
-                    <path d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z"></path>
-                    <path d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z"></path>
-                </svg></i>
-            Add a card
-        </button> -->
     </article>
-    <section v-else class="add-group-container">
-        <add-group-or-task :groupOrTask="'group'" />
+    <section v-else class="new-group-container">
+        <section class="add-group-container">
+            <add-group-or-task :groupOrTask="'group'" />
+        </section>
     </section>
 </template>
 
