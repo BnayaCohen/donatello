c1ac31279421260518fb05549891e322d627e347
Bnaya <bnaya98@gmail.com>
2022-07-21 16:07:01 +0300

    side bar

--- src/services/util-service.js
+++ src/services/util-service.js
@@ -7,7 +7,7 @@ export const utilService = {
   timeAgo,
   makeRandNum,
   getRandomColor,
-  applyDrag,
+  // applyDrag,
 };
 
 function makeRandNum() {
@@ -88,16 +88,16 @@ function getRandomColor() {
   return color;
 }
 
-export const applyDrag = (arr, dragResult) => {
-  const { removedIndex, addedIndex, payload } = dragResult;
-  if (removedIndex === null && addedIndex === null) return arr;
-  const result = [...arr];
-  let itemToAdd = payload;
-  if (removedIndex !== null) {
-    itemToAdd = result.splice(removedIndex, 1)[0];
-  }
-  if (addedIndex !== null) {
-    result.splice(addedIndex, 0, itemToAdd);
-  }
-  return result;
-};
+// export const applyDrag = (arr, dragResult) => {
+//   const { removedIndex, addedIndex, payload } = dragResult;
+//   if (removedIndex === null && addedIndex === null) return arr;
+//   const result = [...arr];
+//   let itemToAdd = payload;
+//   if (removedIndex !== null) {
+//     itemToAdd = result.splice(removedIndex, 1)[0];
+//   }
+//   if (addedIndex !== null) {
+//     result.splice(addedIndex, 0, itemToAdd);
+//   }
+//   return result;
+// }
