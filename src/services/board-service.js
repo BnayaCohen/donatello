69d7cb4239e3ed79396c1ec64275425288bf94d1
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-21 15:12:47 +0300

    task-details labels styling

--- src/services/board-service.js
+++ src/services/board-service.js
@@ -333,7 +333,7 @@ async function removeTask(boardId, groupId, taskId) {
 
   const group = board.groups.find((group) => group.id === groupId)
   const idx = group.tasks.findIndex((task) => task.id === taskId)
-  if (idx !== -1) group.splice(idx, 1)
+  if (idx !== -1) group.tasks.splice(idx, 1)
   return await saveBoard(board)
 }
 
