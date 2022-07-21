4e328db05331c821cb71ad6c3a418dc6edeb404c
Bnaya <bnaya98@gmail.com>
2022-07-21 16:41:56 +0300

    side menu

---
+++ src/cmps/activity-preview.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="cativity-preview">
+        <img :src="activity.byMember.imgUrl" :alt="activity.byMember.fullname">
+        <h3 class="member-fullname">{{}}</h3>
+    </li>
+</template>
+<script>
+export default {
+    name: 'activity-preview',
+    props: {
+        activity: Object,
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    computed: {
+
+    },
+    components: {
+        activityPreview
+    }
+}
+</script>
