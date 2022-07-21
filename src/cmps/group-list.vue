0d889729f8f71f032460cb2cdba4c3ab116a92b6
BarBronshtein <barbronsthein@gmail.com>
2022-07-21 13:29:44 +0300

    added template

--- src/cmps/group-list.vue
+++ src/cmps/group-list.vue
@@ -1,34 +1,56 @@
 <template>
-    <section class="group-list-container flex">
-        <group-preview v-for="group in groups" :group="group" :key="group.id" />
-
-        <article class="new-group">
-            <group-preview :group="newGroup" />
-        </article>
-    </section>
+  <section class="group-list-container flex">
+    <Container
+      class="h-full flex overflow-x-auto gap-8 p-8"
+      group-name="cols"
+      tag="div"
+      orientation="horizontal"
+      @drop="onGroupDrop($event)"
+    >
+      <Draggable v-for="group in groups" :group="group" :key="group.id">
+        <group-preview :group="group" />
+      </Draggable>
+      <article class="new-group">
+        <group-preview :group="newGroup" />
+      </article>
+    </Container>
+  </section>
 </template>
 
 <script>
+import { Container, Draggable } from 'vue3-smooth-dnd'
+import { applyDrag } from '../services/util-service'
 import groupPreview from './group-preview.vue'
 import { boardService } from '../services/board-service.js'
 
 export default {
-    props: {
-        groups: Array,
+  props: {
+    groups: Array,
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    newGroup() {
+      return boardService.getEmptyGroup()
     },
-    data() {
-        return {
-        }
+    scene() {
+      return this.$store.getters.scene
     },
-    computed: {
-        newGroup() {
-            return boardService.getEmptyGroup()
-        }
+  },
+  methods: {
+    onGroupDrop(dropResult) {
+      const scene = Object.assign({}, this.scene)
+      scene.children = applyDrag(scene.children, dropResult)
+      this.scene = scene
+      console.log('dropResult:', dropResult)
+      console.log('scene:', scene)
     },
-    methods: {
-    },
-    components: {
-        groupPreview,
-    },
+  },
+  components: {
+    groupPreview,
+    Container,
+    Draggable,
+  },
 }
 </script>
