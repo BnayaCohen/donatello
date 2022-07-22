d1ad2600e0e08cd956a2c807dbc033eebf8757d4
BarBronshtein <barbronsthein@gmail.com>
2022-07-22 17:50:42 +0300

    change styling

--- src/views/boards-page.vue
+++ src/views/boards-page.vue
@@ -5,6 +5,30 @@
         <div class="board-title-icon"></div>
         <h3>Starred boards</h3>
       </div>
+      <ul class="board-list clean-list">
+        <span
+          v-for="board in starredBoards"
+          class="board-page-display"
+          :key="board._id"
+          @click="openTask(board._id)"
+        >
+          <li
+            :style="board.style"
+            style="height: 150px; width: 150px"
+            @mouseenter="showRemoveBtn(board._id, true)"
+            @mouseleave="hideRemoveBtn"
+          >
+            <span
+              v-show="board._id === removeBtn && starred"
+              class="remove-board-btn"
+              href="#"
+              @click.stop="removeBoard(board._id)"
+            >
+              <i class="fa-regular fa-trash-can"></i
+            ></span>
+          </li>
+        </span>
+      </ul>
     </section>
     <section class="boards-container">
       <div class="boards-title flex">
@@ -23,11 +47,10 @@
             style="height: 150px; width: 150px"
             @mouseenter="showRemoveBtn(board._id)"
             @mouseleave="hideRemoveBtn"
-            @click="setBoard(board._id)"
           >
             {{ board._id }}
             <span
-              v-show="board._id === removeBtn"
+              v-show="board._id === removeBtn && !starred"
               class="remove-board-btn"
               href="#"
               @click.stop="removeBoard(board._id)"
@@ -47,35 +70,36 @@ export default {
   data() {
     return {
       removeBtn: null,
-    };
+      starred: false,
+    }
   },
 
   methods: {
     setBoard(boardId) {
-      this.$store.dispatch({ type: 'loadBoard', boardId });
+      this.$store.dispatch({ type: 'loadBoard', boardId })
     },
-    showRemoveBtn(boardId) {
-      this.removeBtn = boardId;
+    showRemoveBtn(boardId, starred = false) {
+      this.removeBtn = boardId
+      this.starred = starred
     },
     hideRemoveBtn() {
-      this.removeBtn = null;
+      this.removeBtn = null
     },
     removeBoard(boardId) {
-      this.$store.dispatch({ type: 'removeBoard', boardId });
+      this.$store.dispatch({ type: 'removeBoard', boardId })
     },
     openTask(boardId) {
-      this.$router.push('/board/' + boardId);
+      this.$router.push('/board/' + boardId)
     },
   },
   computed: {
     starredBoards() {
-      //   this.$store.getters.starredBoards
-      return false;
+      return this.$store.getters.starredBoards
     },
     boards() {
-      return this.$store.getters.boardsForDisplay;
+      return this.$store.getters.boardsForDisplay
     },
   },
   components: {},
-};
+}
 </script>
