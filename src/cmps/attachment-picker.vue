11a3e8fe461e39ee22c0d67fe371ce15a240afd8
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-22 10:51:34 +0300

    attachment feature added

---
+++ src/cmps/attachment-picker.vue
@@ -0,0 +1,65 @@
+<template>
+  <button class="sidebar-btn flex align-center" @click="toggleAttachment">
+    <span class="trellicons trellicons-attachment"></span>
+    <span>Attachments</span>
+  </button>
+
+  <div
+    ref="attachmentContainer"
+    class="dynamic-popover pos-absolute attachment-popover-container"
+  >
+    <div class="popover-header flex justify-center align-center">
+      <h4>Attach from..</h4>
+      <button class="pop-close-btn" @click="toggleAttachment">
+        <span class="trellicons trellicons-close-btn"></span>
+      </button>
+    </div>
+    <div class="popover-content">
+      <div class="attachment-pop-over-content">
+        <div class="upload-preview">
+          <label for="file-upload">Computer</label
+          ><input
+            type="file"
+            accept="img/*"
+            id="file-upload"
+            ref="fileInput"
+            @input="pickFile"
+          />
+        </div>
+        <h3 class="small-title">From web</h3>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      previewImage: null,
+    }
+  },
+  created() {},
+  methods: {
+    selectImage() {
+      this.$refs.fileInput.click()
+    },
+    pickFile() {
+      let input = this.$refs.fileInput
+      let file = input.files
+      if (file && file[0]) {
+        let reader = new FileReader()
+        reader.onload = (e) => {
+          this.previewImage = e.target.result
+          this.$emit('attachSelected', this.previewImage)
+        }
+        reader.readAsDataURL(file[0])
+      }
+    },
+    toggleAttachment() {
+      const elCover = this.$refs.attachmentContainer
+      elCover.classList.toggle('show')
+    },
+  },
+}
+</script>
+<style></style>
