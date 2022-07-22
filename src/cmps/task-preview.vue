169552082c01159fc12381353a2589830d1d57b8
Bnaya <bnaya98@gmail.com>
2022-07-23 01:32:12 +0300

    design

--- src/cmps/task-preview.vue
+++ src/cmps/task-preview.vue
@@ -9,32 +9,19 @@
       <span v-if="onHover" @click.stop="openSideBar" class="hover-edit-btn"
         ><i class="trellicons trellicons-edit"></i
       ></span>
-      <div class="task-preview-header">
-        <div v-if="task?.styles" class="image-wrapper"></div>
-      </div>
+      <div v-if="task.style?.bgColor" class="task-preview-header" :style="{backgroundColor:task.style.bgColor}">
+        </div>
+        <!-- <div v-if="task?.style" class="image-wrapper">
+      </div> -->
+      <task-label-list v-if="task.labelIds?.length" :labelIds="task.labelIds" />
       <p>{{ task?.title }}</p>
     </div>
   </div>
+  
   <div v-show="isOpen" class="quick-card-editor">
     <span
       @click="closeSideBar"
-      class="icon-lg icon-close quick-card-editor-close-icon"
-      ><svg
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fas"
-        data-icon="xmark"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 320 512"
-        class="svg-inline--fa fa-xmark fa-xl"
-      >
-        <path
-          fill="currentColor"
-          d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
-          class=""
-        ></path>
-      </svg>
+      class="icon-lg icon-close quick-card-editor-close-icon trellicons trellicons-close-btn">
     </span>
     <div :style="getCords" class="quick-card-editor-card">
       <div
@@ -147,6 +134,8 @@
   </div>
 </template>
 <script>
+import taskLabelList from './task-label-list.vue'
+
 export default {
   name: 'taskPreview',
   props: {
@@ -187,6 +176,9 @@ export default {
     },
   },
   emits: ['click'],
+  components:{
+    taskLabelList
+  }
 }
 </script>
 <style></style>
