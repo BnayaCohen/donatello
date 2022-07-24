48e0dd68c78cb256bd9c47660300e6349e8a1f16
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-24 13:19:49 +0300

    .

---
+++ frontend/src/cmps/avatar-preview.vue
@@ -0,0 +1,20 @@
+<template>
+    <img :class="avatarSizeClass" :src="img" :alt="member.fullname">
+</template>
+<script>
+export default {
+    name: 'avatar-preview',
+    props: {
+        member: Object,
+        avatarSize: String
+    },
+    computed: {
+        img() {
+            return this.member.imgUrl ? this.member.imgUrl : 'https://cdn2.iconfinder.com/data/icons/audio-16/96/user_avatar_profile_login_button_account_member-1024.png'
+        },
+        avatarSizeClass() {
+            return 'member-avatar-' + this.avatarSize
+        }
+    },
+}
+</script>
---
+++ frontend/src/cmps/task-details-cmps/label-picker.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="dynamic-popover pos-absolute" :style="pos">
+    <div class="popover-header flex justify-center align-center">
+      <h4>Labels</h4>
+      <button class="pop-close-btn" @click.stop="$emit('closeLabels')">
+        <span class="trellicons trellicons-close-btn"></span>
+      </button>
+    </div>
+    <div class="popover-content label-list-container">
+      <div class="search-labels">
+        <input type="text" placeholder="Search labels..." />
+      </div>
+      <h3 class="small-title">Labels</h3>
+      <ul class="clean-list">
+        <li
+          v-for="label in labels"
+          :key="label.id"
+          @click.stop="addLabel(label.id)"
+          class="label flex align-center"
+        >
+          <div
+            :style="{
+              borderRadius: '3px',
+              width: '100%',
+              height: '32px',
+              backgroundColor: label.color,
+            }"
+          >
+            <span
+              v-if="isLabelInUse(label.id)"
+              class="trellicons trellicons-v-check"
+              :style="{
+                float: 'right',
+                color: '#fff',
+                margin: '7px 7px 0 0',
+                fontWeight: '400',
+              }"
+            ></span>
+          </div>
+          <span class="pos-absolute">{{ label.title }}</span>
+        </li>
+        <div class="label-create">
+          <button class="full-btn">Create a new label</button>
+        </div>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    labels: Array,
+    taskLabels: Array,
+    pos: Object
+  },
+  data() {
+    return {}
+  },
+  created() {
+    console.log(this.pos)
+  },
+  computed: {
+    selectedLabel() {
+      this.taskLabels.forEach((taskLabel) => {
+        if (taskLabel.id === labelId) return 'label-selected'
+        else return ''
+      })
+    },
+  },
+  methods: {
+    isLabelInUse(labelId) {
+      let inUse = false
+      this.taskLabels.forEach((taskLabel) => {
+        if (labelId === taskLabel.id) {
+          inUse = true
+        }
+      })
+      return inUse
+    },
+    addLabel(labelId) {
+      this.$emit('addLabel', labelId)
+    },
+  },
+  emits: ['addLabel'],
+}
+</script>
+<style></style>
