07f90942d589a92cc149379559fb326bd96a1445
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-25 22:33:31 +0300

    user-menu added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div :style="bgStyle">
-    <app-header />
+    <app-header @toggleUserMenu="toggleUserMenu"/>
     <router-view @setBackground="initBackground" />
   </div>
+  <user-menu
+    v-if="isUserMenuOpen"
+    @toggleUserMenu="toggleUserMenu"
+    v-click-outside="toggleUserMenu"
+    :pos="getCords"
+  />
 </template>
 
 <script>
 import appHeader from '@/cmps/app-header.vue'
+import userMenu from '@/cmps/user-menu.vue'
 
 export default {
   emits: ['setBackground'],
@@ -14,6 +21,9 @@ export default {
   data() {
     return {
       background: '',
+      isUserMenuOpen: false,
+      x: 0,
+      y: 0,
     }
   },
   created() {
@@ -21,8 +31,12 @@ export default {
   },
   methods: {
     initBackground(background) {
-      console.log(this.$route)
       this.background = background
+    },
+    toggleUserMenu(ev) {
+      this.x = ev?.clientX
+      this.y = ev?.clientY
+      this.isUserMenuOpen = !this.isUserMenuOpen
     },
   },
   computed: {
@@ -31,9 +45,13 @@ export default {
         background: this.background,
       }
     },
+    getCords() {
+      return { top: this.y + 30 + 'px', left: this.x - 278 + 'px' }
+    },
   },
   components: {
     appHeader,
+    userMenu
   },
 }
 </script>
