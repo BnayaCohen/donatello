1f6d95ad90650bc78af500bfabf4b4055b83feaa
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-26 22:27:25 +0300

    duedate style and remove button fixes

--- src/cmps/date.vue
+++ src/cmps/date.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="pos-absolute due-date-picker" :style="pos">
+  <div class="dynamic-popover pos-absolute due-date-picker" :style="pos">
+    <div class="popover-header flex justify-center align-center">
+      <span>Date</span>
+      <button class="pop-close-btn" @click.stop="$emit('toggleDate', $event)">
+        <span class="trellicons trellicons-close-btn"></span>
+      </button>
+    </div>
+
     <datepicker
-      :inline="true"
+      inline
       class="due-date-picker"
       id="due-date-picker"
       v-model="dueDate"
       :value="dueDate"
       @update:modelValue="updateDueDate"
     ></datepicker>
+    <div class="pop-content">
+      <button
+        class="full-btn flex align-center justify-center save-duedate-btn"
+        @click.stop="$emit('toggleDate', $event)"
+      >
+        <span>Save</span>
+      </button>
+      <button
+        class="full-btn flex align-center justify-center remove-duedate-btn"
+        @click.stop="removeDueDate"
+      >
+        <span>Remove</span>
+      </button>
+    </div>
   </div>
 </template>
 <script>
@@ -23,8 +44,13 @@ export default {
     updateDueDate() {
       this.$emit('updateDueDate', this.dueDate)
     },
+    removeDueDate() {
+      this.$emit('removeDueDate')
+      this.$emit('toggleDate')
+    }
   },
   components: { Datepicker },
+  emits: ['updateDueDate', 'removeDueDate', 'toggleDate']
 }
 </script>
 <style></style>
