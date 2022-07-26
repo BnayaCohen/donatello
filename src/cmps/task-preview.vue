bf6aaf41494d95b33f620d2e7004c3bad8e48d4a
BarBronshtein <barbronshtein@gmail.com>
2022-07-26 21:50:12 +0300

    added quick edit styling

--- src/cmps/task-preview.vue
+++ src/cmps/task-preview.vue
@@ -1,19 +1,10 @@
 <template>
-  <div
-    class="task-preview"
-    @mouseenter="toggleOnHover"
-    @mouseleave="toggleOnHover"
-    @click.stop="openTask(task.groupId, task.id)"
-  >
+  <div class="task-preview" @mouseenter="toggleOnHover" @mouseleave="toggleOnHover"
+    @click.stop="openTask(task.groupId, task.id)">
     <div class="flex flex-column">
-      <span v-if="onHover" @click.stop="openQuickEdit" class="hover-edit-btn"
-        ><i class="trellicons trellicons-edit"></i
-      ></span>
-      <div
-        v-if="task.style?.background"
-        class="task-preview-header"
-        :style="task.style"
-      >
+      <span v-if="onHover" @click.stop="openQuickEdit" class="hover-edit-btn"><i
+          class="trellicons trellicons-edit"></i></span>
+      <div v-if="task.style?.background" class="task-preview-header" :style="task.style">
         <img class="task-img-cover" :src="task.style.background" alt="" />
       </div>
       <task-label-list v-if="task.labelIds?.length" :labelIds="task.labelIds" />
@@ -21,28 +12,16 @@
 
       <section class="task-indicators-container">
         <div class="flex">
-          <div
-            v-if="task.dueDate"
-            class="task-due-date"
-            @mouseenter="toggleDueDateOnHover"
-            @mouseleave="toggleDueDateOnHover"
-            @click.stop="toggleDueDateCheck"
-            :style="dueDateStyle"
-          >
+          <div v-if="task.dueDate" class="task-due-date" @mouseenter="toggleDueDateOnHover"
+            @mouseleave="toggleDueDateOnHover" @click.stop="toggleDueDateCheck" :style="dueDateStyle">
             <div class="due-date-icon">
-              <i
-                class="trellicons"
-                :class="'trellicons-' + getDueDateIconName"
-              ></i>
+              <i class="trellicons" :class="'trellicons-' + getDueDateIconName"></i>
             </div>
             <p>{{ getFixedDueDate }}</p>
           </div>
 
           <section class="task-indicators">
-            <span
-              v-if="task.description"
-              class="task-indicator trellicons trellicons-description"
-            ></span>
+            <span v-if="task.description" class="task-indicator trellicons trellicons-description"></span>
             <div v-if="task.comments?.length" class="task-indicator">
               <span class="trellicons trellicons-comment"></span>
               <p>{{ task.comments.length }}</p>
@@ -58,140 +37,18 @@
           </section>
         </div>
         <section v-if="task.memberIds?.length" class="task-members-container">
-          <avatar-preview
-            v-for="memberId in task.memberIds"
-            :key="memberId"
-            :member="getMemberById(memberId)"
-            :avatarSize="'small'"
-          />
+          <avatar-preview v-for="memberId in task.memberIds" :key="memberId" :member="getMemberById(memberId)"
+            :avatarSize="'small'" />
         </section>
       </section>
     </div>
   </div>
-
-  <div v-show="isOpen" class="quick-card-editor">
-    <span
-      class="icon-lg icon-close quick-card-editor-close-icon trellicons trellicons-close-btn"
-    >
-    </span>
-    <div
-      :style="getCords"
-      v-click-outside="closeQuickEdit"
-      class="quick-card-editor-card"
-    >
-      <div
-        :style="task?.styles"
-        class="list-card list-card-quick-edit is-covered"
-      >
-        <div class="list-card-details">
-          <div v-if="task?.labels?.length" class="list-card-labels">
-            <span
-              v-for="label in task?.labels"
-              :key="label.id"
-              :style="`backgroundColor:${label.color}`"
-              >{{ label }}</span
-            >
-          </div>
-          <textarea
-            data-autosize="true"
-            dir="auto"
-            style="
-              overflow: hidden;
-              overflow-wrap: break-word;
-              resize: none;
-              height: 90px;
-            "
-          ></textarea>
-          <div class="badges">
-            <span class="badges-container">
-              <div v-if="task?.isWatched" class="badge badge-watch">
-                <i class="fa-regular fa-eye"></i>
-              </div>
-              <div v-if="task?.comments?.length" class="badge comments">
-                <i class="fa-regular fa-message"></i
-                ><span>{{ task?.comments.length }}</span>
-              </div>
-              <div v-if="task?.attachment?.length" class="badge attachments">
-                <i class="fa-regular fa-link"></i
-                ><span>{{ task?.attachment.length }}</span>
-              </div>
-            </span>
-          </div>
-          <div v-if="task?.members?.length" class="list-card-members">
-            <div
-              v-for="member in task.members"
-              :key="member._id"
-              class="member"
-              :style="`backgroundImage:url(${member.imgUrl}),border-radius:50%,height:30px,width:30px`"
-            ></div>
-          </div>
-        </div>
-      </div>
-      <button class="save-task-btn text-center">Save</button>
-      <div class="quick-card-editor-buttons fade-in">
-        <span
-          class="quick-card-editor-buttons-item"
-          @click.stop="openTask(task.groupId, task.id)"
-        >
-          <span class="icon-sm icon-card"
-            ><i class="fa-solid fa-inbox"></i></span
-          ><span class="quick-card-editor-buttons-item-text">Open card</span>
-        </span>
-        <span href="#" class="quick-card-editor-buttons-item"
-          ><span class="icon-sm icon-label"
-            ><i class="fa-solid fa-tag"></i></span
-          ><span class="quick-card-editor-buttons-item-text"
-            >Edit Labels</span
-          ></span
-        >
-        <span href="#" class="quick-card-editor-buttons-item">
-          <span class="icon-sm icon-member"
-            ><i class="fa-regular fa-user"></i
-          ></span>
-          <span class="quick-card-editor-buttons-item-text"
-            >Change members</span
-          ></span
-        >
-        <span href="#" class="quick-card-editor-buttons-item"
-          ><span class="icon-sm icon-card-cover"
-            ><i class="fa-solid fa-inbox"></i
-          ></span>
-          <span class="quick-card-editor-buttons-item-text"
-            >Change cover</span
-          ></span
-        >
-        <span href="#" class="quick-card-editor-buttons-item">
-          <span class="icon-sm icon-move"
-            ><i class="fa-solid fa-arrow-right"></i
-          ></span>
-          <span class="quick-card-editor-buttons-item-text">Move</span>
-        </span>
-        <span href="#" class="quick-card-editor-buttons-item">
-          <span class="icon-sm icon-card"
-            ><i class="fa-solid fa-inbox"></i
-          ></span>
-          <span class="quick-card-editor-buttons-item-text">Copy</span>
-        </span>
-        <span href="#" class="quick-card-editor-buttons-item">
-          <span class="icon-sm icon-clock"
-            ><i class="fa-regular fa-clock"></i
-          ></span>
-          <span class="quick-card-editor-buttons-item-text">Edit dates</span>
-        </span>
-        <span href="#" class="quick-card-editor-buttons-item">
-          <span class="icon-sm icon-archive"
-            ><i class="fa-solid fa-box-archive"></i
-          ></span>
-          <span class="quick-card-editor-buttons-item-text">Archive</span>
-        </span>
-      </div>
-    </div>
-  </div>
+  <quick-edit @closeQuickEdit="closeQuickEdit" :getCords="getCords" v-show="isOpen" :task="task" />
 </template>
 <script>
 import taskLabelList from './task-label-list.vue'
 import avatarPreview from './avatar-preview.vue'
-
+import quickEdit from './quick-edit.vue'
 export default {
   name: 'taskPreview',
   props: {
@@ -262,14 +119,14 @@ export default {
                 ? '#eb5a46'
                 : '#ec9488'
               : this.onDueDateHover
-              ? '#00000014'
-              : '#00000000'
+                ? '#00000014'
+                : '#00000000'
             : this.onDueDateHover
-            ? '#519839'
-            : '#61bd4f',
+              ? '#519839'
+              : '#61bd4f',
         color:
           Date.now() - this.task.dueDate > 0 ||
-          this.task.status !== 'in-progress'
+            this.task.status !== 'in-progress'
             ? '#fff'
             : '#5e6c84',
       }
@@ -298,7 +155,9 @@ export default {
   components: {
     taskLabelList,
     avatarPreview,
+    quickEdit
   },
 }
 </script>
-<style></style>
+<style>
+</style>
