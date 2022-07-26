69e929e0ad85ae717538bafe3a414b06e2e56b22
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-26 12:48:39 +0300

    .

---
+++ src/cmps/task-details-cmps/comment-preview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="comment-container">
+    <div class="task-detail-title">
+      <span class="trellicons trellicons-comments"></span>
+      <h3>Activity</h3>
+    </div>
+    <div class="new-comment-container flex align-center">
+        <div class="member-icon-container">
+            <img class="member-avatar-big" />
+        </div>
+        <form>
+            <div class="comment-frame">
+                <div class="comment-box">
+                    <textarea class="comment-box-input"></textarea>
+                </div>
+            </div>
+        </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    loggedInUser: Object
+  }
+}
+</script>
+<style></style>
