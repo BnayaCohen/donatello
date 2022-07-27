6a472961eb08544199d336390b020fe630545492
BarBronshtein <barbronshtein@gmail.com>
2022-07-27 19:21:37 +0300

    added start demo in home page

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -5,22 +5,20 @@
       <div class="hero-description">
         <div class="text-container">
           <h1>
-            Donatello helps teams move work
-            <span ref="el" data-words='["forward.","together.","better."]'
-              ><span class="txt"></span
-            ></span>
+            Donatello helps teams move work forward.
+            <span ref="el" data-words='["forward.","together.","better."]'><span class="txt"></span></span>
           </h1>
           <p>
             Collaborate,manage projects and reach new productivity peaks. from
             high rises to the home office,the way your team works is unique
             —accomplish it all with Donatello.
           </p>
+          <a class="start-doing" @click="loginAsGuest"><span>Start demo</span></a>
         </div>
       </div>
       <div class="hero-img">
         <img
-          src="https://images.ctfassets.net/rz1oowkt5gyp/5QIzYxue6b7raOnVFtMyQs/113acb8633ee8f0c9cb305d3a228823c/hero.png?w=1200&fm=webp"
-        />
+          src="https://images.ctfassets.net/rz1oowkt5gyp/5QIzYxue6b7raOnVFtMyQs/113acb8633ee8f0c9cb305d3a228823c/hero.png?w=1200&fm=webp" />
       </div>
     </div>
     <section class="get-started">
@@ -30,11 +28,10 @@
         more features as your teamwork grows. Manage projects, organize tasks,
         and build team spirit—all in one place.
       </p>
-      <a class="start-doing" @click="loginAsGuest"><span>Start doing</span></a>
+
     </section>
     <img
-      src="https://images.ctfassets.net/rz1oowkt5gyp/7pYWhpQ3vnntxoShaImNws/777fabbf069416489167ab92027ce086/board.png?w=1200&fm=webp"
-    />
+      src="https://images.ctfassets.net/rz1oowkt5gyp/7pYWhpQ3vnntxoShaImNws/777fabbf069416489167ab92027ce086/board.png?w=1200&fm=webp" />
   </section>
 </template>
 
@@ -42,11 +39,11 @@
 import headerHomePage from '../cmps/header-page.vue'
 import { TypeWriter } from '../services/util-service'
 export default {
-  mounted() {
-    const el = this.$refs.el
-    const words = JSON.parse(el.getAttribute('data-words'))
-    new TypeWriter(el, words)
-  },
+  // mounted() {
+  //   const el = this.$refs.el
+  //   const words = JSON.parse(el.getAttribute('data-words'))
+  //   new TypeWriter(el, words)
+  // },
   components: { headerHomePage },
   data() {
     return {
@@ -55,8 +52,8 @@ export default {
   },
   methods: {
     loginAsGuest() {
-      const credentials = {username: 'guest', password: 'guest'}
-      this.$store.dispatch({type: 'login', credentials})
+      const credentials = { username: 'guest', password: 'guest' }
+      this.$store.dispatch({ type: 'login', credentials })
       this.$router.push('/board')
     }
   }
@@ -68,6 +65,7 @@ export default {
   display: grid;
   grid-template-columns: 10vw 1fr 10vw;
 }
+
 .home-page-layout * {
   grid-column: 2;
 }
